30fc600380247955688d7b3eacb9a45b12b7aabc
CJ Smith <cjs.connor.smith@gmail.com>
2017-03-09 19:21:58 -0500

    Update EightBit_Noise_Smith.java

--- EightBit_Noise_Smith.java
+++ EightBit_Noise_Smith.java
@@ -7,27 +7,27 @@ import java.util.Random;
 public class EightBit_Noise_Smith implements PlugInFilter
 {
     ImagePlus im = IJ.getImage();
-    public int setup(String arg, ImagePlus im)
-    {
- return DOES_8G; //accepts 8-bits gray scale
-    }
-    public void run(ImageProcessor ip)
-    {
- int w = ip.getWidth();
- int h = ip.getHeight();
- float p =0;
- Random rand_p = new Random();
- //iterate
- for(int u = 0;u<w;u++)
- {
- for(int v = 0;v<h;v++)
- {
- p =    (float)ip.getPixel(u, v);
- //This value changes to change the standard deviation
- p = p + (float)(rand_p.nextGaussian()*25);
- ip.set(u, v, Math.round(p));
- }
- }
- im.updateAndDraw();
-    }
+        public int setup(String arg, ImagePlus im)
+        {
+            return DOES_8G; //accepts 8-bits gray scale
+        }
+        public void run(ImageProcessor ip)
+        {
+            int w = ip.getWidth();
+            int h = ip.getHeight();
+            float p =0;
+            Random rand_p = new Random();
+                //iterate
+                for(int u = 0;u<w;u++)
+                {
+                    for(int v = 0;v<h;v++)
+                    {
+                        p =    (float)ip.getPixel(u, v);
+                        //This value changes to change the standard deviation
+                        p = p + (float)(rand_p.nextGaussian()*25);
+                        ip.set(u, v, Math.round(p));
+                    }
+                }
+            im.updateAndDraw();
+        }
 }
