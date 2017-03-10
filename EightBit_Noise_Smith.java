6ac5a6469e5993938255bfc76919c0df867e330a
CJ Smith <cjs.connor.smith@gmail.com>
2017-03-09 19:19:20 -0500

    Create EightBit_Noise_Smith.java

---
+++ EightBit_Noise_Smith.java
@@ -0,0 +1,33 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import java.lang.Math;
+import java.util.Random;
+public class EightBit_Noise_Smith implements PlugInFilter
+{
+    ImagePlus im = IJ.getImage();
+    public int setup(String arg, ImagePlus im)
+    {
+ return DOES_8G; //accepts 8-bits gray scale
+    }
+    public void run(ImageProcessor ip)
+    {
+ int w = ip.getWidth();
+ int h = ip.getHeight();
+ float p =0;
+ Random rand_p = new Random();
+ //iterate
+ for(int u = 0;u<w;u++)
+ {
+ for(int v = 0;v<h;v++)
+ {
+ p =    (float)ip.getPixel(u, v);
+ //This value changes to change the standard deviation
+ p = p + (float)(rand_p.nextGaussian()*25);
+ ip.set(u, v, Math.round(p));
+ }
+ }
+ im.updateAndDraw();
+    }
+}
